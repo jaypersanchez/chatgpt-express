e228d7a278eaa1212b2a9969a0dee0454512b745
jaypersanchez <jaypersanchez@gmail.com>
2023-03-07 10:24:39 +0800

    empty functions

--- routes/chatgpt.js
+++ routes/chatgpt.js
@@ -1,26 +1,61 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config();
-//var { Configuration, OpenAIApi } from "openai";
-var ai = require('openai')
-const configuration = new ai.Configuration({
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
     organization: process.env.REACT_APP_ORGANIZATION_KEY,
-    apiKey: process.env.CHATGPT_KEY,
+    apiKey: process.env.CHATGPT_KEY2,
 });
-const openai = new ai.OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(`ChatGPT ${process.env.REACT_APP_ORGANIZATION_KEY} :: ${process.env.CHATGPT_KEY}`);
+  res.send(`ChatGPT ${configuration.apiKey} :: ${configuration.organization}`);
 });
 
 router.get('/listmodels', function(req, res, next) {
-    const response = openai.listModels();
-    res.send(JSON.stringify(response))
+    openai.listModels()
+    .then(result => {
+        res.send(result.data)
+    }) 
 })
 
-router.get('/completions', function(req, res, next) {
-    //const response = openai.createCompletion()
+router.get('/textcompletion', function(req, res, next) {
+    getTextCompletion(req)
+    .then((result) => {
+        console.log(result)
+        res.send(result)
+    })
 })
+
+router.get('/codecompletion', function(req, res) {
+
+})
+
+router.get('/chatcompletion', function(req, res) {
+
+})
+
+/* Code Completion */
+const getCodeCompletion = async(req) => {
+
+}
+
+/* Chat Completion */
+const getChatCompletion = async(req) => {
+
+}
+
+/* Text Completion */
+const getTextCompletion = async (req) => {
+    var completion = await openai.createCompletion({
+        model: req.body.model,
+        prompt: req.body.prompt,
+        temperature: req.body.temperature,
+        max_tokens: req.body.max_tokens,
+        max_tokens: 500, 
+    })
+    return(completion.data.choices[0].text)
+}
 
 module.exports = router;
